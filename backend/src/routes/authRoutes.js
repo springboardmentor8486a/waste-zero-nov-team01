b383496a7ef6c09fd652710efab94009b418c9b4
Sankha Singhamahapatra <sankha.s.m.patra03@gmail.com>
2025-12-30 22:15:28 +0530

    Milestone 3 Backend - Completed.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,14 @@
-// routes/authRoutes.js
 const express = require("express");
 const router = express.Router();
-const { register, login, changePassword, getMe } = require("../controllers/authController");
+
+const { register, login, getMe } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
 
+// PUBLIC routes
 router.post("/register", register);
 router.post("/login", login);
-router.post("/change-password", protect, changePassword); // important
+
+// PRIVATE route (requires token)
 router.get("/me", protect, getMe);
 
 module.exports = router;
